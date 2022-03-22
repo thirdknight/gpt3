ac37881946785bc0e1b64f16b7f89a943b347afe
Edinson <edinson.ningles@gmail.com>
2022-03-22 16:48:37 +0100

    Add Header component with styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3} from './containers';
 import { Brand, CTA, Navbar } from './components';
-import styles from './App.module.css';
+import styles from './App.css';
 
 const App = () => {
   return(
     <div className='App'>
-      <div className={styles.gradientBg}>
+      <div className={`gradientBg`}>
         <Navbar />
         <Header />
       </div>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import styles from './header.module.css';
+import React from "react";
+import styles from "./header.module.css";
+import '../../App.css';
+
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className={`sectionPadding ${styles.gpt3Header}`}>
+      <div className={styles.gpt3HeaderContent}>
+        <h1 className={`gradientText`}>
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Let’s Build Something amazing with GPT-3 OpenAI Yet bed any for
+          travelling assistance indulgence unpleasing. Not thoughts all exercise
+          blessing. Indulgence way everything joy alteration boisterous the
+          attachment. Party we years to order allow asked of. Your Email Address
+          Get Started 1.6k+ 1,600 people requested access a visit in last 24
+          hours
+        </p>
+        <div className={styles.gpt3HeaderContentInput}>
+          <input type="email" placeholder="Your Email Adress" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className={styles.gpt3HeaderContentPeople}>
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className={styles.gpt3HeaderImage}>
+          <img src={ai} alt="ai" />  
+        </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
