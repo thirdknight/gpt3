7ba2c1bf7b56039732bf848019e4b204963990eb
Edinson <edinson.ningles@gmail.com>
2022-03-23 17:58:34 +0100

    Add gpt3whatgpt3 container

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import styles from './feature.module.css';
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div>Feature</div>
+    <div className={styles.gpt3FeaturesContainerFeature}>
+      <div className={styles.gpt3FeaturesContainerFeatureTitle}>
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className={styles.gpt3FeaturesContainerFeatureText}>
+          {text}
+        </div>
+    </div>
   )
 }
 
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import styles from './whatGPT3.module.css';
+import React from "react";
+import { Feature } from "../../components";
+import styles from "./whatGPT3.module.css";
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
-  )
-}
+    <div className={`sectionMargin ${styles.gpt3Whatgpt3}`} id="whpt3">
+      <div className={styles.gpt3Whatgpt3Feature}>
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      </div>
+      <div className={styles.gpt3Whatgpt3Heading}>
+        <h1 className={`gradientText`}>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className={styles.gpt3Whatgpt3Container}>
+        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. " />
+        <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+        <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      </div>
+    </div>
+  );
+};
 
-export default WhatGPT3
+export default WhatGPT3;
